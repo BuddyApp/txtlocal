25d369e9f061518d1f88aa3346238b374b4643f7
Omar Qureshi <omar@omarqureshi.net>
2014-01-22 19:37:40 +0000

    Get received messages count from API also

--- lib/txtlocal/report.rb
+++ lib/txtlocal/report.rb
@@ -1,20 +1,34 @@
 module Txtlocal
   class Report
-    API_ENDPOINT = URI.parse("https://api.txtlocal.com/get_history_api/")
+    SENT_API_ENDPOINT = URI.parse("https://api.txtlocal.com/get_history_api/")
+    RECEIVED_API_ENDPOINT = URI.parse("https://api.txtlocal.com/get_messages/?")
 
     def initialize(options={})
       options[:days] ||= 1
-      res = Txtlocal::Report.fetch_for_days(options[:days])
-      return res.size
+      sent_messages = Txtlocal::Report.fetch_for_days(options[:days], :sent)
+      result = {:sent => sent_messages.size}
+      if options[:inbox_id]
+        received_messages = Txtlocal::Report.fetch_for_days(options[:days], :received, options[:inbox_id])
+        result = result.merge({:received => received_messages.size})
+      end
+      result
     end
 
-    def self.fetch_for_days(days)
+    def self.fetch_for_days(days, type, inbox_id=nil)
       from_timestamp = Time.now.to_i
       til_timestamp = (Date.today - days).to_time.to_i
       last_result = nil
       result = []
       while true
-        current_result = fetch(from_timestamp, til_timestamp)
+        current_result = case type
+                         when :sent
+                           fetch_sent_messages(from_timestamp, til_timestamp)
+                         when :received
+                           raise "Must supply inbox id" if inbox_id.nil?
+                           fetch_received_messages(from_timestamp, til_timestamp, inbox_id)
+                         else
+                           raise "Incorrect type"
+                         end
         if current_result.nil? || current_result.empty?
           break
         else
@@ -28,11 +42,11 @@ module Txtlocal
       result
     end
 
-    def self.fetch(max_time, min_time)
-      http = Net::HTTP.new(API_ENDPOINT.host, API_ENDPOINT.port)
+    def self.fetch_sent_messages(max_time, min_time)
+      http = Net::HTTP.new(SENT_API_ENDPOINT.host, SENT_API_ENDPOINT.port)
       http.use_ssl = true
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      req = Net::HTTP::Post.new(API_ENDPOINT.path)
+      req = Net::HTTP::Post.new(SENT_API_ENDPOINT.path)
       req.set_form_data(:format => "json",
                         :max_time => max_time,
                         :min_time => min_time,
@@ -45,5 +59,22 @@ module Txtlocal
       JSON.parse(result.body)["messages"]
     end
 
+    def self.fetch_received_messages(max_time, min_time, inbox_id)
+      http = Net::HTTP.new(RECEIVED_API_ENDPOINT.host, RECEIVED_API_ENDPOINT.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      req = Net::HTTP::Post.new(RECEIVED_API_ENDPOINT.path)
+      req.set_form_data(:format => "json",
+                        :max_time => max_time,
+                        :min_time => min_time,
+                        :limit => 1000, # Maximum limit
+                        :sort_order => "asc",
+                        :test => Txtlocal.config.testing? ? 1 : 0,
+                        :username => Txtlocal.config.username,
+                        :inbox_id => inbox_id,
+                        :hash => Txtlocal.config.password)
+      result = http.start { |http| http.request(req) }
+      JSON.parse(result.body)["messages"]
+    end
   end
 end
--- spec/report_spec.rb
+++ spec/report_spec.rb
@@ -2,49 +2,71 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 
 describe Txtlocal::Report do
   context "without api" do
-    it "should call the fetch_for_days method" do
+    it "should call the fetch_for_days method once if inbox_id is not supplied" do
       Txtlocal::Report.should_receive(:fetch_for_days).exactly(1).times.and_return([])
       Txtlocal::Report.new
     end
 
-    it "fetch_for_days should call the fetch method" do
-      Txtlocal::Report.should_receive(:fetch).exactly(1).times.and_return([])
-      Txtlocal::Report.fetch_for_days(1)
+    it "should call the fetch_for_days method once if inbox_id is not supplied" do
+      Txtlocal::Report.should_receive(:fetch_for_days).exactly(2).times.and_return([])
+      Txtlocal::Report.new(:inbox_id => 123)
     end
 
-    it "should call the fetch method twice if fetch returns something" do
-      now = Time.now
-      day_ago = Chronic.parse("1 day ago").to_date.to_time.to_i
-      three_hours_ago = Chronic.parse("3 hours ago")
-      Txtlocal::Report.should_receive(:fetch).with(now.to_i, day_ago.to_i).exactly(1).times.and_return([{"datetime" => three_hours_ago.to_s}])
-      Txtlocal::Report.should_receive(:fetch).with(now.to_i, three_hours_ago.to_i).exactly(1).times.and_return([])
-      Txtlocal::Report.fetch_for_days(1)
+    describe "fetch_for_days" do
+      it "should call the fetch_sent_messages method when looking for sent messages" do
+        Txtlocal::Report.should_receive(:fetch_sent_messages).exactly(1).times.and_return([])
+        Txtlocal::Report.fetch_for_days(1, :sent)
+      end
+
+      it "should call the fetch_received_messages method" do
+        Txtlocal::Report.should_receive(:fetch_received_messages).exactly(1).times.and_return([])
+        Txtlocal::Report.fetch_for_days(1, :received, 123)
+      end
+
+      it "should call the fetch method twice if fetch returns something" do
+        now = Time.now
+        day_ago = Chronic.parse("1 day ago").to_date.to_time.to_i
+        three_hours_ago = Chronic.parse("3 hours ago")
+        Txtlocal::Report.should_receive(:fetch_sent_messages).with(now.to_i, day_ago.to_i).exactly(1).times.and_return([{"datetime" => three_hours_ago.to_s}])
+        Txtlocal::Report.should_receive(:fetch_sent_messages).with(now.to_i, three_hours_ago.to_i).exactly(1).times.and_return([])
+        Txtlocal::Report.should_receive(:fetch_received_messages).with(now.to_i, day_ago.to_i, 123).exactly(1).times.and_return([{"datetime" => three_hours_ago.to_s}])
+        Txtlocal::Report.should_receive(:fetch_received_messages).with(now.to_i, three_hours_ago.to_i, 123).exactly(1).times.and_return([])
+
+        Txtlocal::Report.new(:days => 1, :inbox_id => 123)
+      end
+
     end
+
   end
 
 
   context "api test mode" do
     before(:each) do
-      yaml = YAML.load_file(File.join(File.dirname(__FILE__), '..', 'api_login.yml'))
+      @yaml = YAML.load_file(File.join(File.dirname(__FILE__), '..', 'api_login.yml'))
       WebMock.allow_net_connect!
       Txtlocal.config do |c|
         c.from = "testing"
-        c.username = yaml["api_username"]
-        c.password = yaml["api_password"]
+        c.username = @yaml["api_username"]
+        c.password = @yaml["api_password"]
         c.test = true
       end
     end
 
     it "should combine records over a period of time when given multiple days" do
-      Txtlocal::Report.fetch_for_days(3)
+      Txtlocal::Report.fetch_for_days(3, :sent)
+      Txtlocal::Report.fetch_for_days(3, :received, @yaml["received_inbox_id"])
     end
 
-    it "should fetch records when given a max time and min time" do
-      Txtlocal::Report.fetch(Time.now.to_i, Chronic.parse("1 day ago").to_i)
+    it "should fetch sent records when given a max time and min time" do
+      Txtlocal::Report.fetch_sent_messages(Time.now.to_i, Chronic.parse("1 day ago").to_i)
+    end
+
+    it "should fetch received records when given a max time and min time" do
+      Txtlocal::Report.fetch_received_messages(Time.now.to_i, Chronic.parse("1 day ago").to_i, @yaml['received_inbox_id'])
     end
 
     it "should receive data from the api endpoint" do
-      Txtlocal::Report.new
+      Txtlocal::Report.new(:inbox_id => @yaml['received_inbox_id'])
     end
   end
 end
